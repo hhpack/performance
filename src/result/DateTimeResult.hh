899ae5e5f8e00329579018b1919c4b6f85ee6068
holyshared <holy.shared.design@gmail.com>
2015-11-01 19:42:36 +0900

    add watcher

---
+++ src/result/DateTimeResult.hh
@@ -0,0 +1,48 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\stopwatch.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\stopwatch\result;
+
+use hhpack\stopwatch\Range;
+use DateInterval;
+use DateTimeImmutable;
+
+final class DateTimeResult implements Range<DateTimeImmutable, DateInterval>
+{
+
+    public function __construct(
+        private DateTimeImmutable $startAt = new DateTimeImmutable(),
+        private DateTimeImmutable $stoppedAt = new DateTimeImmutable()
+    )
+    {
+    }
+
+    public function getStartedValue() : DateTimeImmutable
+    {
+        return $this->startAt;
+    }
+
+    public function getStoppedValue() : DateTimeImmutable
+    {
+        return $this->stoppedAt;
+    }
+
+    public function diff() : DateInterval
+    {
+        return $this->stoppedAt->diff($this->startAt);
+    }
+
+    public static function createFrom(Pair<DateTimeImmutable, DateTimeImmutable> $range) : this
+    {
+        return new static($range[0], $range[1]);
+    }
+
+}
