1b042ff1fe1e121739f6dd799765a2dad1f621c0
holyshared <holy.shared.design@gmail.com>
2016-01-24 18:10:23 +0900

    add interface

--- src/result/ComplexResult.hh
+++ src/result/ComplexResult.hh
@@ -12,38 +12,75 @@
 namespace hhpack\performance\result;
 
 use hhpack\performance\WatchedResult;
+use ConstMapAccess;
 
-final class ComplexResult implements WatchedResult<ImmMap<string, num>>
+final class ComplexResult implements WatchedResult<ImmMap<string, num>>, ConstMapAccess<string, num>
 {
 
-    private ImmMap<string, WatchedResult<num>> $results;
+    private ImmMap<string, num> $result;
+    private ImmMap<string, WatchedResult<num>> $watchedResult;
 
     public function __construct(
         KeyedTraversable<string, WatchedResult<num>> $results = []
     )
     {
-        $namedResult = Map {};
+        $result = Map {};
+        $watchedResult = Map {};
 
         foreach ($results as $key => $value) {
-            $namedResult->set($key, $value);
+            $result->set($key, $value->result());
+            $watchedResult->set($key, $value);
         }
-        $this->results = $namedResult->toImmMap();
+        $this->result = $result->toImmMap();
+        $this->watchedResult = $watchedResult->toImmMap();
     }
 
+    <<__Memoize>>
     public function first() : ImmMap<string, num>
     {
-        return $this->results->map(($result) ==> $result->first());
+        return $this->watchedResult->map(($result) ==> $result->first());
     }
 
+    <<__Memoize>>
     public function last() : ImmMap<string, num>
     {
-        return $this->results->map(($result) ==> $result->last());
+        return $this->watchedResult->map(($result) ==> $result->last());
     }
 
     <<__Memoize>>
     public function result() : ImmMap<string, num>
     {
-        return $this->results->map(($result) ==> $result->result());
+        return $this->toImmMap();
+    }
+
+    public function mapWithKey<Tu>((function(string,num):Tu) $mapper) : ImmMap<string, Tu>
+    {
+        return $this->result->mapWithKey($mapper);
+    }
+
+    public function contains<Tu super string>(Tu $m) : bool
+    {
+        return $this->result->contains($m);
+    }
+
+    public function at(string $k) : num
+    {
+        return $this->result->at($k);
+    }
+
+    public function get(string $k) : ?num
+    {
+        return $this->result->get($k);
+    }
+
+    public function containsKey<Tu super string>(Tu $k): bool 
+    {
+        return $this->result->containsKey($k);
+    }
+
+    public function toImmMap() : ImmMap<string, num>
+    {
+        return $this->result->toImmMap();
     }
 
 }
