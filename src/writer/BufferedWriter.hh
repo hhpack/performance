2fd61e542ff309824027001cadc96401a797e209
holyshared <holy.shared.design@gmail.com>
2016-01-30 17:36:42 +0900

    add writer

---
+++ src/writer/BufferedWriter.hh
@@ -0,0 +1,36 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\performance.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\performance\writer;
+
+use hhpack\performance\Writer;
+
+final class BufferedWriter implements Writer<void>
+{
+
+    private string $content = '';
+
+    public function write(string $value) : void
+    {
+        $this->content .= $value;
+    }
+
+    public function writeln(string $value) : void
+    {
+        $this->content .= $value . PHP_EOL;
+    }
+
+    public function __toString() : string
+    {
+        return $this->content;
+    }
+
+}
---
+++ src/writer/StdoutWriter.hh
@@ -0,0 +1,29 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\performance.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\performance\writer;
+
+use hhpack\performance\Writer;
+
+final class StdoutWriter implements Writer<void>
+{
+
+    public function write(string $value) : void
+    {
+        fwrite(STDOUT, $value);
+    }
+
+    public function writeln(string $value) : void
+    {
+        $this->write($value . PHP_EOL);
+    }
+
+}
